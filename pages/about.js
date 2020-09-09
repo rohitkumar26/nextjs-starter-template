027dfdf440a9a5f8d17d477713f1c989e675ab99
Coder Rohit <rkumar26384@gmail.com>
2020-09-09 16:22:03 +0530

    first commit

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div>
+            <h1>i am about page</h1>
+        </div>
+    )
+}
+
+export default About
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import Link from 'next/link'
+const Index = () => (
+    <div>
+        <h1>Home page</h1>
+        <Link href="/about">About Page</Link>
+    </div>
+)
+
+export default Index
